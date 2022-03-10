dda0439b3cfc0db82c755a4216140a821931776f
Ilyos Khurozov <ilyos.developer@gmail.com>
2022-03-10 11:41:26 +0500

    added Router

--- src/main/java/uz/ilyoskhurozov/anyroute/Controller.java
+++ src/main/java/uz/ilyoskhurozov/anyroute/Controller.java
@@ -4,8 +4,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import uz.ilyoskhurozov.anyroute.component.Router;
+
 
 public class Controller {
+    private int r=0;
+
+    @FXML
+    private AnchorPane desk;
 
     @FXML
     private ToggleButton routerBtn;
@@ -26,5 +34,13 @@ public class Controller {
 
         algorithms.getItems().addAll("Dijskstra", "algorithm 2", "algorithm 3", "algorithm 4", "algorithm 5");
         algorithms.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    void mouseClickOnDesk(MouseEvent e){
+        if (routerBtn.isSelected() && e.getTarget().equals(desk)){
+            Router router = new Router(++r, e.getX(), e.getY());
+            desk.getChildren().add(router);
+        }
     }
 }
---
+++ src/main/java/uz/ilyoskhurozov/anyroute/component/Router.java
@@ -0,0 +1,81 @@
+package uz.ilyoskhurozov.anyroute.component;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import org.kordamp.ikonli.javafx.FontIcon;
+
+public class Router extends VBox {
+    // router position
+    private double x = 0;
+    private double y = 0;
+    // mouse position
+    private double mouseX = 0;
+    private double mouseY = 0;
+
+    private final Label label;
+    private final FontIcon icon;
+
+    public Router(int number, double x, double y){
+        icon = new FontIcon("mdi2r-router");
+        icon.setIconSize(30);
+        label = new Label("R"+number);
+        setColor(Color.BLACK);
+
+        getChildren().addAll(icon, label);
+
+        setLayoutX(x);
+        setLayoutY(y);
+        setAlignment(Pos.CENTER);
+
+        makeHoverable();
+        makeDraggable();
+    }
+
+    private void setColor(Color color){
+        icon.setIconColor(color);
+        label.setTextFill(color);
+    }
+
+    private void makeHoverable(){
+        setOnMouseEntered(mouseEvent -> setColor(Color.STEELBLUE));
+        setOnMouseExited(mouseEvent -> setColor(Color.BLACK));
+    }
+
+    private void makeDraggable(){
+        setOnMousePressed(event -> {
+            setColor(Color.BLACK);
+
+            mouseX = event.getSceneX();
+            mouseY = event.getSceneY();
+
+            x = getLayoutX();
+            y = getLayoutY();
+        });
+
+        setOnMouseDragged(event -> {
+            double offsetX = event.getSceneX() - mouseX;
+            double offsetY = event.getSceneY() - mouseY;
+
+            x += offsetX;
+            y += offsetY;
+
+            double maxX = getParent().getLayoutBounds().getMaxX();
+            double maxY = getParent().getLayoutBounds().getMaxY();
+
+            if (x >= 0 && x + getWidth() <= maxX) setLayoutX(x);
+            if (y >= 0 && y + getHeight() <= maxY) setLayoutY(y);
+
+            mouseX = event.getSceneX();
+            mouseY = event.getSceneY();
+
+            event.consume();
+        });
+    }
+
+    @Override
+    public String toString() {
+        return "Router{"+label.getText()+"}";
+    }
+}
