d7648e86430f826563b034141f17c9d9f7b816e7
ulugbe7 <ulugbekyusupov272@gmail.com>
2023-08-01 18:05:11 +0500

    change name

--- src/main/java/uz/ilyoskhurozov/anyroute/App.java
+++ src/main/java/uz/ilyoskhurozov/anyroute/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("app.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Any route");
+        stage.setTitle("Fuzzy route");
         stage.setScene(scene);
         stage.getIcons().add(new Image(Objects.requireNonNull(App.class.getResourceAsStream("/images/gps.png"))));
         stage.setMaximized(true);
